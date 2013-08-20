0895dfd149b917db7bef7d9bd0d43a6a62280153
head-thrash <ilya.cherkasov@gmail.com>
2013-08-20 18:38:58 +0400

    Trying for problem 18

--- eulers1to25.py
+++ eulers1to25.py
@@ -545,9 +545,37 @@ def problem17():
     )
 
 
+def problem18():
+    X = [[75],
+         [95, 64],
+         [17, 47, 82],
+         [18, 35, 87, 10],
+         [20, 4, 82, 47, 65],
+         [19, 1, 23, 75, 3, 34],
+         [88, 2, 77, 73, 7, 63, 67],
+         [99, 65, 4, 28, 6, 16, 70, 92],
+         [41, 41, 26, 56, 83, 40, 80, 70, 33],
+         [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+         [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+         [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+         [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+         [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+         [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
+
+    res = 75
+    traverse_index = 0
+    for i in range(1, len(X)):
+        arr = X[i]
+        maximum = max(arr[traverse_index], arr[traverse_index + 1])
+        print(maximum)
+        traverse_index = arr.index(maximum)
+        res += maximum
+    print(res) # wrong!
+
+
 start = default_timer()
 
-problem17()
+problem18()
 
 print('Elapsed:{}'.format(default_timer() - start))
 
